0469efe8b58480f53d2390cc461378069c3a700f
Lida Tang <lida.tang@gmail.com>
2012-10-14 19:21:46 -0400

    make sure the column and panel have ids

--- lib/active_admin/views/columns.rb
+++ lib/active_admin/views/columns.rb
@@ -1,18 +1,24 @@
 module ActiveAdmin
   module Views
-    class Columns
-      alias :add_child_without_id :add_child
-
-      def add_child(child)
-        @column_count ||= 0
-        add_child_without_id child
-        # add an id by default
-        if (child.is_a?(Arbre::Element) && child.id.nil?)
-          child.id = id+"_c#{@column_count}"
-          @column_count += 1
-        end
+    class Column
+      alias :build_without_id :build
+      
+      def build(*args)
+        bild_without_id *args
+        id!
+      end
+    end
+    
+    class Panel
+      alias :build_without_id :build
+      
+      def build(*args)
+        bild_without_id *args
+        id!
       end
       
     end
+    
   end
-end      
+end
+
