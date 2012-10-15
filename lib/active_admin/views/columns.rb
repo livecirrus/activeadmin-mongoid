336aa19f052a510458e89dfc626a26b1531e8226
Lida Tang <lida.tang@gmail.com>
2012-10-14 20:07:01 -0400

    changed how id is calculated to be based on when the component is built and counting based on parent's children

--- lib/active_admin/views/columns.rb
+++ lib/active_admin/views/columns.rb
@@ -1,31 +1,27 @@
 module ActiveAdmin
   module Views
-    class Columns
-      alias :add_child_without_id :add_child
+    class Column
+      alias :build_without_default_id :build
 
-      def add_child(child)
-        @column_count ||= 0
-        add_child_without_id child
-        # add an id by default
-        if (child.is_a?(Arbre::Element) && child.id.nil?)
-          child.id = (id || '')+"_c#{@column_count}"
-          @column_count += 1
+      def build(options={})
+        count = parent.children.count do |child|
+           child.is_a? Column
         end
+        options[:id] = "#{parent.id || ''}_c#{count}" if options[:id].nil?
+        build_without_default_id(options)
       end      
     end
 
-    class Column
-      alias :add_child_without_id :add_child
+    class Panel
+      alias :build_without_default_id :build
 
-      def add_child(child)
-        @panel_count ||= 0
-        add_child_without_id child
-        # add an id by default
-        if (child.is_a?(Arbre::Element) && child.id.nil?)
-          child.id = (id || '')+"_p#{@panel_count}"
-          @panel_count += 1
+      def build(title, attributes={})
+        count = parent.children.count do |child|
+           child.is_a? Panel
         end
-      end      
+        attributes[:id] = "#{parent.id || ''}_p#{count}" if attributes[:id].nil?
+        build_without_default_id(title, attributes)
+      end            
     end
   end
 end      
